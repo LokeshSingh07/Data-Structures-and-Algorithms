e1c27a7739960091f24b5e165b4e30268e5c37f3
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-10-02 01:24:06 +0530

    Arrays

--- 01 Arrays/3_find_unique_element.cpp
+++ 01 Arrays/3_find_unique_element.cpp
@@ -1,3 +1,10 @@
+/*
+    Given an array of elements occurring in multiples of k, except one element which doesn't occur in multiple of k. Return the unique element.
+
+    Input: k = 3, arr[] = [6, 2, 5, 2, 2, 6, 6]
+    Output: 5
+    Explanation: Every element appears 3 times except 5.
+*/ 
 class Solution {
   public:
     int findUnique(int k, vector<int>& arr) {
@@ -11,3 +18,27 @@ class Solution {
         }
     }
 };
+
+
+
+
+// Every element occur two times, except one element
+#include<bits/stdc++.h>
+using namespace std;
+
+int findUnique(int arr[], int n){
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+
+int main(){
+    int arr[] = {2,10,22,10,33,4,33,4,2};
+    int n = 9;
+    cout<<findUnique(arr, n);
+return 0;
+}
+
--- 01 Arrays/4_shift_array_by_1.cpp
+++ 01 Arrays/4_shift_array_by_1.cpp
@@ -9,3 +9,25 @@ class Solution {
         arr[0] = temp;
     }
 };
+
+
+
+
+// Rotate array by K    --  TLE 
+class Solution {
+public:
+    void shiftBy1(vector<int> &nums){
+        int n = nums.size();
+        int temp = nums[n-1];
+        for(int i=n-1; i>0; i--){
+            nums[i] = nums[i-1];
+        } 
+        nums[0] = temp;
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        while(k--)
+            shiftBy1(nums);
+    }
+};
