77bed034579efff2945969055ec8ad3ecd3196cf
Lokesh Singh <122585548+LokeshSingh07@users.noreply.github.com>
2025-01-10 23:57:10 +0530

    Arrays
    TUF

---
+++ 01 Arrays/TUF/05_remove_duplicate_from_sorted_array.cpp
@@ -0,0 +1,20 @@
+// 26. Remove Duplicates from Sorted Array
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i=0, j=0;
+
+        while(j<n){
+            if(nums[i] == nums[j]) j++;
+            else{
+                i++;
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+
+        return i+1;
+    }
+};
